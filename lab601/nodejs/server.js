1a9cf40868f9c5eeb2bf16215d5413cd8e0c41ed
Martin Jones <martin@ignition-training.com>
2024-04-05 00:28:28 -0400

    Internal build and push added

---
+++ lab601/nodejs/server.js
@@ -0,0 +1,30 @@
+"use strict"
+
+const express = require('express')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+app.use(cookieParser())
+
+const listenHost = "0.0.0.0"
+const listenPort =  parseInt(process.env.LISTEN_PORT) || 3344 
+const greeting = process.env.GREETING || "Hello"
+
+const userCookie = 'authdaemon2-user'
+
+app.get('*', function(req, res) {
+  try {
+    const user = req.cookies[userCookie] || 'UnknownUser'
+
+    res.send(`${greeting} ${user}. The current time is: ${new Date()}`)
+  }
+  catch (err) {
+    console.error(`Error processing ${err} ${JSON.stringify(err)}`)
+    res.status(503).send(`Error processing ${req.path}`)
+  }
+})
+
+app.listen(listenPort, listenHost, function () {
+  console.log(`Server listening on ${listenHost}:${listenPort}`)
+})
+
