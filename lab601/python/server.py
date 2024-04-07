ced632232276ba91fb7c37b0a0d717d3fd6177ee
Martin Jones <martin@ignition-training.com>
2024-04-07 17:41:50 -0400

    Lab 601 Python and nodejs manifest changes

---
+++ lab601/python/server.py
@@ -0,0 +1,26 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from http.cookies import BaseCookie
+import datetime
+import os
+
+hostName = "0.0.0.0"
+serverPort = int(os.environ['LISTEN_PORT']) if os.environ['LISTEN_PORT'] else 4574
+greeting = os.environ['GREETING'] if os.environ['GREETING'] else 'Namaste'
+
+userCookieName = 'authdaemon2-user'
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        bc = BaseCookie(self.headers['Cookie'])
+        userCookie = bc.get(userCookieName)
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(bytes(f"{greeting} {userCookie.value if userCookie else 'Unknown' }. The current time is {datetime.datetime.now()}", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print(f"Server listening on {hostName}:{serverPort}")
+
+    webServer.serve_forever()
+
