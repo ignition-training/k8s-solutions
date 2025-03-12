4c4ee7242301edc1b5e6e71fcfcd404a42ef08c1
Martin Jones <martin@ignition-training.com>
2025-03-12 00:38:28 +0000

    Updates JS packages, uses module syntax, and removes 501 pull secrets

--- lab501/nodejs/server.js
+++ lab501/nodejs/server.js
@@ -1,7 +1,5 @@
-"use strict"
-
-const express = require('express')
-const cookieParser = require('cookie-parser')
+import express from 'express'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 app.use(cookieParser())
